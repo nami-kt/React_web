82e120a01499cc5db35743c334a773e4a16b55b0
nami-kt <43203057+nami-kt@users.noreply.github.com>
2018-11-21 11:18:46 +0100

    Commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,27 @@
 import React from "react";
 
 export default class Content extends React.Component {
+  constructor() {
+    super();
+    this.state = { counter: 0 };
+  }
+
   render() {
     return (
       <div>
-        <button>Click content</button>
+        Uz jste klikli: {this.state.counter} krat
+        <br />
+        <button onClick={e => this.handleClick(e)}>Click content</button>
+        <button onWheel={e => this.handleWheel(e)}>Wheel content</button>
       </div>
     );
+  }
+
+  handleClick(e) {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  handleWheel(e) {
+    this.setState({ counter: this.state.counter * 13 + 1 });
   }
 }
